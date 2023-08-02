d34094c1d6647307883bc165a62a91be4b747f24
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-02 17:36:03 +0530

    [Mukesh] Add : Validation for first name

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Markup;
+using UserRegistrationProblem;
+
+namespace RegexOperations
+{
+    public class Program
+    {
+        static string firstName = "Mukesh";
+
+        private static void Main(string[] args)
+        {
+            UserRegistration validate = new UserRegistration();
+            Console.WriteLine(firstName);
+            validate.ValidateName(firstName);
+
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UserRegistration
+    {
+        string nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+
+        public void ValidateName(string name)
+        {
+            if (Regex.IsMatch(name, nameRegex))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+        }
+    }
+}
