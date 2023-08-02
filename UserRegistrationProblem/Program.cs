99714411eb115b59c63ac1952c2f63febb9ea0d1
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-02 17:43:05 +0530

    [Mukesh] Add : Validation for last name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -7,12 +7,15 @@ namespace RegexOperations
     public class Program
     {
         static string firstName = "Mukesh";
+        static string lastName = "Mukkara";
 
         private static void Main(string[] args)
         {
             UserRegistration validate = new UserRegistration();
             Console.WriteLine(firstName);
             validate.ValidateName(firstName);
+            Console.WriteLine(lastName);
+            validate.ValidateName(lastName);
 
         }
     }
